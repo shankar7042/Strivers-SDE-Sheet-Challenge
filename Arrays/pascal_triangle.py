6478552b7c091002e91565daac1220f6872d268a
shankar7042 <shankar.kumar.4171@gmail.com>
2023-06-06 19:39:12 +0530

    q2: other approach

--- Arrays/pascal_triangle.py
+++ Arrays/pascal_triangle.py
@@ -1,4 +1,4 @@
-def printPascal(n: int):
+def printPascal1(n: int) -> list[list[int]]:
     ans = [[1]]
 
     for i in range(1, n):
@@ -10,9 +10,29 @@ def printPascal(n: int):
     return ans
 
 
+def generateRow(row: int) -> list[int]:
+    ans = 1
+    ansRow = list()
+    ansRow.append(ans)
+
+    for col in range(1, row):
+        ans = ans * (row - col)
+        ans = ans // col
+        ansRow.append(ans)
+    return ansRow
+
+
+def printPascal2(n: int) -> list[list[int]]:
+    triangle = list()
+    for i in range(1, n+1):
+        triangle.append(generateRow(i))
+    return triangle
+
+
 def printPascalTriangle(pascalMatrix: list[int]):
     for i in range(len(pascalMatrix)):
         print(pascalMatrix[i])
 
 
-printPascalTriangle(printPascal(10))
+printPascalTriangle(printPascal2(10))
+# print(generateRow(10))
