d1758bf3713facbc6c605a1e61627051bbe29d80
shankar7042 <shankar.kumar.4171@gmail.com>
2023-06-07 23:17:20 +0530

    rotate matrix by 90 deg

--- Arrays-II/rotate_matrix_clockwise.py
+++ Arrays-II/rotate_matrix_clockwise.py
@@ -1,9 +1,9 @@
 # n = row, m = col
+# Rotate Matrix clockwise by 1
 def rotateMatrix(mat: list[list[int]], n: int, m: int):
     row_start, col_start, row_end, col_end = 0, 0, n-1, m-1
 
     while row_start < row_end and col_start < col_end:
-        print(row_start, col_start, row_end, col_end)
         last_ele = mat[row_start][col_end]
         for i in range(col_end, col_start, -1):
             mat[row_start][i] = mat[row_start][i-1]
@@ -27,18 +27,27 @@ def rotateMatrix(mat: list[list[int]], n: int, m: int):
             1, col_start+1, row_end-1, col_end-1
 
 
+# Rotate Matrix by 90 deg
+def rotate(matrix: list[list[int]]) -> None:
+    n = len(matrix)
+    m = len(matrix[0])
+    # Transpose
+    for i in range(n):
+        for j in range(i):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    # revese each row to get the final matrix
+    for i in range(n):
+        matrix[i].reverse()
+
+
 def printMatrix(matrix):
     for row in matrix:
         print(row)
 
 
-arr = [
-    [1, 2, 3, 4],
-    [5, 6, 7, 8],
-    [9, 10, 11, 12],
-    [13, 14, 15, 16]
-]
-rotateMatrix(arr, len(arr), len(arr[0]))
+arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+rotate(arr)
 printMatrix(arr)
 
 # the above array should rotate clock wise ans output will be like below
