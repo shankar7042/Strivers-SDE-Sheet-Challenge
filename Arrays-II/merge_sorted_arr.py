4a276ec9f785869d45dccd70e9ceb007706c6b5e
shankar7042 <shankar.kumar.4171@gmail.com>
2023-06-11 10:28:15 +0530

    q9: merge two sorted arrays shell sort method

--- Arrays-II/merge_sorted_arr.py
+++ Arrays-II/merge_sorted_arr.py
@@ -17,7 +17,39 @@ def mergeSortedArray(arr1: list[int], arr2: list[int], m: int, n: int):
     arr2.sort()
 
 
-arr1 = [1, 2, 3, 0, 0]
-arr2 = [4, 5]
-mergeSortedArray(arr1, arr2, len(arr1), len(arr2))
+def swap_if_greater(arr1: list[int], arr2: list[int], ind1: int, ind2: int):
+    if arr1[ind1] > arr2[ind2]:
+        arr1[ind1], arr2[ind2] = arr2[ind2], arr1[ind1]
+
+
+# gap method / shell sort method
+
+
+def mergeSortedArray2(arr1: list[int], arr2: list[int], m: int, n: int):
+    totalLen = m + n
+    gap = (totalLen // 2) + (totalLen % 2)
+    while gap > 0:
+        left = 0
+        right = left + gap
+        while right < totalLen:
+            # left -> arr1 and right -> arr2
+            if left < m and right >= m:
+                swap_if_greater(arr1, arr2, left, right - m)
+            # left -> arr2 and right -> arr2
+            elif left >= m:
+                swap_if_greater(arr2, arr2, left - m, right - m)
+            # left -> arr1 and right -> arr1
+            elif right < m:
+                swap_if_greater(arr1, arr1, left, right)
+            left += 1
+            right += 1
+
+        if gap == 1:
+            break
+        gap = (gap // 2) + (gap % 2)
+
+
+arr1 = [10, 20, 30, 40, 50]
+arr2 = [5, 15, 25, 35, 45, 55, 65]
+mergeSortedArray2(arr1, arr2, len(arr1), len(arr2))
 print(arr1, arr2)
